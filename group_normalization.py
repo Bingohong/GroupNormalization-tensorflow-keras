9bf4d5dd1a551e41ae05e3b7cb9d562302550310
bingoHong <hgbhtt@163.com>
2018-04-21 20:32:00 +0800

    Update group_normalization.py

--- group_normalization.py
+++ group_normalization.py
@@ -27,7 +27,6 @@ def GroupNormalization(x, phase_train, groups=32, decay=0.99, scope="gn"):
 	ExponentialMovingAverage method
 	-------------------------------
 	- apply()方法添加了训练变量的影子副本，并保持了其影子副本中训练变量的移动平均值操作。在每次训练之后调用此操作，更新移动平均值。
-
 	- average()和average_name()方法可以获取影子变量及其名称。
 	- EMA 操作不会改变变量本身的值，而是维护一个影子变量记录其滑动平均值，当需要使用这个滑动平均值时可以使用average函数
 
@@ -44,7 +43,7 @@ def GroupNormalization(x, phase_train, groups=32, decay=0.99, scope="gn"):
 	    x = tf.transpose(x, [0, 3, 1, 2])
 	    N, C, H, W = x.get_shape().as_list()
 	    G = min(G, C)
-	    x = tf.reshape(x, [N, G, C // G, H, W])
+	    x = tf.reshape(x, [-1, G, C // G, H, W])
 
 	    # per channel gamma and beta
 	    gamma = tf.get_variable('gamma', [C], initializer=tf.constant_initializer(1.0))
@@ -56,13 +55,13 @@ def GroupNormalization(x, phase_train, groups=32, decay=0.99, scope="gn"):
 	    gn_mean, gn_var = tf.nn.moments(x, [2, 3, 4], keep_dims=True, name="moments")
 
 	    # ema ops to record global mean & var update
-	    ema = tf.ExponentialMovingAverage(decay=decay)
+	    ema = tf.train.ExponentialMovingAverage(decay=decay)
 	    ema_mean, ema_var = ema.average(gn_mean), ema.average(gn_var)
 
 	    def mean_var_with_update():
 	    	# add shadow variable
 	    	ema_apply_op = ema.apply([gn_mean,gn_var])
-	    	with tf.control_dependency([ema_apply_op]):
+	    	with tf.control_dependencies([ema_apply_op]):
 	    		return tf.identity(gn_mean), tf.identity(gn_var)
 
 	    # depend on phase_train 
@@ -70,9 +69,9 @@ def GroupNormalization(x, phase_train, groups=32, decay=0.99, scope="gn"):
 	    # when phase train if False, return ema_mean & ema_var
 	    mean, var = tf.cond(phase_train, mean_var_with_update, lambda:(ema_mean,ema_var))
 
-		# compute group normalization
-		x = (x - mean) / tf.sqrt(var + esp)
-	    output = tf.reshape(x, [N, C, H, W]) * gamma + beta
+	    # compute group normalization
+	    x = (x - mean) / tf.sqrt(var + esp)
+	    output = tf.reshape(x, [-1, C, H, W]) * gamma + beta
 
 	    # tranpose: [bs, c, h, w, c] to [bs, h, w, c] following the paper
 	    output = tf.transpose(output, [0, 2, 3, 1])
